21147954eb84c7a74ea0f3fd870bd5696a73f5f2
yenchiachen <charlisyenchiachen@gmail.com>
2024-04-07 21:43:27 +0800

    initial: landing page

---
+++ src/src/pages/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className="bg-[#d4d4d4] min-h-screen">
+        <div className='container mx-auto'>
+            <h1 className='py-16 text-3xl font-black'>Some Useful React Components...</h1>
+        </div>
+    </div>
+  );
+}
+
+export default LandingPage;
